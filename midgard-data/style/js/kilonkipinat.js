842c694c010c4e91514a7d703f80f20f2ea67d0f
netblade <oskari.kokko@iki.fi>
2010-05-07 01:07:25 +0300

    corners

--- midgard-data/style/js/kilonkipinat.js
+++ midgard-data/style/js/kilonkipinat.js
@@ -10,8 +10,8 @@ var ApplicationBase = function()
     
     var _buildCorners = function() {
         jQuery('.corners15').corner("round 15px").parent().css('padding', '5px 5px 5px 5px').corner("round 15px");
-		jQuery('.corners10').corner("round 10px").parent().css('padding', '5px 5px 5px 5px').corner("round 15px");
-		jQuery('.corners5').corner("round 5px").parent().css('padding', '5px 5px 5px 5px').corner("round 15px");
+		jQuery('.corners10').corner("round 10px").parent().css('padding', '5px 5px 5px 5px').corner("round 10px");
+		jQuery('.corners5').corner("round 5px").parent().css('padding', '5px 5px 5px 5px').corner("round 5px");
     };
 
     return {
