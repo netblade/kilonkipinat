648fd7415f66b17850dd46922f6671a391a862db
netblade <oskari.kokko@iki.fi>
2010-05-15 12:28:34 +0300

    show downloads also in extranet

--- midgard-data/filesync/style/kkp-2009/extranet/show-article.php
+++ midgard-data/filesync/style/kkp-2009/extranet/show-article.php
@@ -2,7 +2,53 @@
 // Available request keys: article, datamanager, edit_url, delete_url, create_urls
 $view = $data['view_article'];
 ?>
-
+<(article-notes)>
 <h1>&(view['title']:h);</h1>
 
 &(view['content']:h);
+
+<?php
+    if(isset($data['datamanager']->types["downloads"]))
+    {
+        $tmp_downloads = $data['datamanager']->types["downloads"]->attachments_info;
+    }
+    if (   isset($tmp_downloads)
+        && count($tmp_downloads)>0)
+    {
+?>
+<div class="downloads">
+    <h3>Tiedostot</h3>
+    <ul>
+<?php
+        foreach($tmp_downloads as $tmp_download)
+        {
+            $filesize = round(($tmp_download['filesize'] / 1024), 0);
+            if($filesize > 1000)
+            {
+                $filesize = round(($filesize / 1024), 2) . ' MB';
+            }
+            else
+            {
+                $filesize = $filesize . ' kb';
+            }
+            $filetype = str_replace('application/', '', $tmp_download['mimetype']);
+            $filetype = str_replace('image/', '', $filetype);
+            if ($filetype != '')
+            {
+                $filetype .= ', ';
+            }
+            if($tmp_download['description'] != '')
+            {
+                echo "\t\t<li><a href=\"" . $tmp_download['url'] . "\">" . $tmp_download['description'] . "</a> (" . $filetype . $filesize . ")</li>\n";
+            }
+            else
+            {
+                echo "\t\t<li><a href=\"" . $tmp_download['url'] . "\">" . $tmp_download['filename'] . "</a> (" . $filetype . $filesize . ")</li>\n";
+            }
+        }
+        ?>
+    </ul>
+</div>
+<?php
+    }
+?>
