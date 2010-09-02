4903a5ff781be68244d92e878e52d6ed191a1280
Oskari Kokko <oskari.kokko@iki.fi>
2010-09-02 11:24:35 +0300

    fixes to events component

--- midgard-data/components/fi.kilonkipinat.events/style/show-event.php
+++ midgard-data/components/fi.kilonkipinat.events/style/show-event.php
@@ -78,27 +78,27 @@ if ($show_location) {
 ?>
         <div id="fi_kilonkipinat_events_event_location">
 <?php
-
-if (   array_key_exists('location', $view)
-    && trim(strip_tags($view['location'])) != '')
-{ 
-    ?>
-        <div class="loc">
-            Osoite: <span class="location">&(view['location']:h);</span>
-        </div>
-    <?php 
-}
+$location_object = $data['object'];
 if (class_exists('org_routamc_positioning_object'))
 {
-    $object_position = new org_routamc_positioning_object($data['event']);
+    $object_position = new org_routamc_positioning_object($location_object);
     $coordinates = $object_position->get_coordinates();
     if (   $coordinates['latitude']
         && $coordinates['longitude'])
     {
+        if (   array_key_exists('location', $view)
+            && trim(strip_tags($view['location'])) != '')
+        { 
+            ?>
+                <div class="loc">
+                    Osoite: <span class="location">&(view['location']:h);</span>
+                </div>
+            <?php 
+        }
         $map = new org_routamc_positioning_map('fi_kilonkipinat_events_event_map');
-        $map->zoom_level = $event->locationzoom;
+        $map->zoom_level = $location_object->locationzoom;
         $marker = array('path' => '/style/img/pin-kkp.png');
-        $map->add_object($event, $marker);
+        $map->add_object($location_object, $marker);
         $map->show(); 
     }
 }
--- midgard-data/components/fi.kilonkipinat.events/style/show-location.php
+++ midgard-data/components/fi.kilonkipinat.events/style/show-location.php
@@ -8,27 +8,27 @@ $view = $data['view_location'];
 &(view['content']:h);
 <div id="fi_kilonkipinat_events_location_location">
 <?php
-
-if (   array_key_exists('location', $view)
-    && trim(strip_tags($view['location'])) != '')
-{ 
-    ?>
-    <div class="loc">
-        Osoite: <span class="location">&(view['location']:h);</span>
-    </div>
-    <?php 
-}
+$location_object = $data['object'];
 if (class_exists('org_routamc_positioning_object'))
 {
-    $object_position = new org_routamc_positioning_object($data['object']);
+    $object_position = new org_routamc_positioning_object($location_object);
     $coordinates = $object_position->get_coordinates();
     if (   $coordinates['latitude']
         && $coordinates['longitude'])
     {
-        $map = new org_routamc_positioning_map('fi_kilonkipinat_events_location_map');
-        $map->zoom_level = $data['object']->locationzoom;
+        if (   array_key_exists('location', $view)
+            && trim(strip_tags($view['location'])) != '')
+        { 
+            ?>
+                <div class="loc">
+                    Osoite: <span class="location">&(view['location']:h);</span>
+                </div>
+            <?php 
+        }
+        $map = new org_routamc_positioning_map('fi_kilonkipinat_events_event_map');
+        $map->zoom_level = $location_object->locationzoom;
         $marker = array('path' => '/style/img/pin-kkp.png');
-        $map->add_object($data['object'], $marker);
+        $map->add_object($location_object, $marker);
         $map->show(); 
     }
 }
