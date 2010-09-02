e572484be8f899ac556600cad32a1f93366740dd
Oskari Kokko <oskari.kokko@iki.fi>
2010-09-02 10:57:02 +0300

    some minor fixes

--- midgard-data/components/fi.kilonkipinat.events/event.php
+++ midgard-data/components/fi.kilonkipinat.events/event.php
@@ -1,6 +1,19 @@
 <?php
+/**
+ * @package fi.kilonkipinat.events
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * @package fi.kilonkipinat.events
+ */
 class fi_kilonkipinat_events_event_dba extends __fi_kilonkipinat_events_event_dba
 {
-    
+    static function &get_cached($src)
+    {
+        return $_MIDCOM->dbfactory->get_cached(__CLASS__, $src);
+    }
 }
 ?>
